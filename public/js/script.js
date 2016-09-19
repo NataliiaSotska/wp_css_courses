61dc6e73ccc7c02c8d7d4189b95ea4c0f02e7988
Nataliia <nataliasotskaja@gmail.com>
2016-09-19 09:52:21 +0300

    Fix all

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,36 @@
-/**
- * Created by 1 on 04.09.2016.
- */
-var $openButton = $('.burger-icon');
-var $openMainHeaderNav = $('.main-header-nav');
-var $closePageWrapper = $('.page-wrapper');
+$(document).ready(function () {
+    var $openButton = $('.burger-icon');
+    var $openMainHeaderNav = $('.main-header-nav');
+    var $closePageWrapper = $('.page-wrapper');
+    var $openSearch = $('.nav-search-btn');
+    var $searchInput = $('input[type="search"]');
 
-$openButton.click(function (event) {
-    event.preventDefault();
+    $openButton.click(function (event) {
+        event.preventDefault();
 
-    if($openMainHeaderNav.hasClass('open') && $closePageWrapper.hasClass('close')) {
-        $openMainHeaderNav.removeClass('open');
-        $closePageWrapper.removeClass('close');
-    } else {
-        $openMainHeaderNav.addClass('open');
-        $closePageWrapper.addClass('close');
-    }
+        if ($openMainHeaderNav.hasClass('open') && $closePageWrapper.hasClass('close')) {
+            $openMainHeaderNav.removeClass('open');
+            $closePageWrapper.removeClass('close');
+        } else {
+            $openMainHeaderNav.addClass('open');
+            $closePageWrapper.addClass('close');
+        }
+    });
+
+    $openSearch.click(function (event) {
+        var $windowWidth = $(window).width();
+        event.preventDefault();
+
+        if ($windowWidth >= '544') {
+            $searchInput.toggle();
+        } else {
+            $searchInput.addClass('small-screen');
+        }
+    });
+
+    $('.intro-bxslider').bxSlider({
+        auto: true,
+        pause: 6000,
+    });
+
 });
